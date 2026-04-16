4ad93a9bf365d9fa575de4fd31ff1e5b5a0f3780
alessandra.dolmeta <alessandra.dolmeta@polito.it>
2026-04-16 11:15:40 +0200

    🧹clear main branch, keeping only README.md

--- scripts/sim/compile_remote_bitbang.sh
+++
@@ -1,4 +0,0 @@
-cd ../../../hw/vendor/esl_epfl_x_heep/hw/vendor/pulp_platform_pulpissimo/rtl/tb/remote_bitbang
-make clean
-make all
-cd -
--- scripts/synthesis/dc_shell/set_constraints.tcl
+++
@@ -1,1 +0,0 @@
-create_clock -name "INPUT_CLK" -period 3.5 [ get_ports clk_i ]
